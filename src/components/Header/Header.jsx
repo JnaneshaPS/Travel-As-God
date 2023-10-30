95f1b1c84a145f5512b800031694909467a61398
Jnanesh <jnaneshps4@gmail.com>
2023-10-30 18:38:49 +0530

    change3

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -12,7 +12,7 @@ const Header = ({ onPlaceChanged, onLoad }) => {
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
         <Typography variant="h5" className={classes.title}>
-          Travel Advisors
+          Travelers Advisory
         </Typography>
         <Box display="flex">
           <Typography variant="h6" className={classes.title}>
